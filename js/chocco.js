f47dc2006f97507ff70d6bf1a876b79ed6dd54a7
Kristina Blixt <kristina-89_08@mail.ru>
2020-06-30 15:24:17 +0200

    js width

--- js/chocco.js
+++ js/chocco.js
@@ -1,10 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
-const menuList = document.querySelector(".menu__list");
+const menuList = document.querySelector(".menu");
 const menuCross = document.querySelector(".menu__cross");
+
 
 hamburger.addEventListener("click", function (e) {
     e.preventDefault();
     menuList.classList.add("menu__opened");
 });
 
-console.log("test");
+menuCross.addEventListener("click", function (e) {
+    menuList.classList.remove("menu__opened");
+});
