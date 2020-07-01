238fd98d568cc25bcd405a66a1f00dad08a6d5fb
Kristina Blixt <kristina-89_08@mail.ru>
2020-07-02 00:58:10 +0200

    js menu

--- js/chocco.js
+++ js/chocco.js
@@ -6,8 +6,23 @@ const menuCross = document.querySelector(".menu__cross");
 hamburger.addEventListener("click", function (e) {
     e.preventDefault();
     menuList.classList.add("menu__opened");
-});
+}
+
+);
 
 menuCross.addEventListener("click", function (e) {
     menuList.classList.remove("menu__opened");
-});
+}
+
+);
+
+
+const displayBox = document.querySelector(".display__box");
+const foodList = document.querySelector(".display__food");
+
+displayBox.addEventListener("click", function (e) {
+    e.preventDefault();
+    foodList.
+}
+
+)
