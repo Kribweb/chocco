52a02815c504dbb024e53bdd863f3797e757caeb
Kristina Blixt <kristina-89_08@mail.ru>
2020-07-09 01:33:55 +0200

    homework

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+$(".form").submit(e => {
+    e.preventDefault();
+
+    $.fancybox.open({
+        src: "#modal",
+        type: "inline"
+    })
+});
+$(".app-submit-btn").click(e => {
+    e.preventDefault();
+    $.fancybox.close();
+});
